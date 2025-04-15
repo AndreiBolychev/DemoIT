0f5844b79ad157d639c4e1dd3cd59b858b111101
andrei <andreiboly4ev@gmail.com>
2025-04-15 15:56:01 +0300

    homework1

---
+++ src/Homework1/Homework1_3.java
@@ -0,0 +1,12 @@
+package Homework1;
+
+public class Homework1_3 {
+    public static void main(String[] args) {
+    int n = 125;
+    int sot = n/100;
+    int des = n/10 %10;
+    int edin = n%10;
+    int sum= sot+des+edin;
+    System.out.println(sum);
+    }
+}
